1cf91130fc830e071817c077fc7c4838f47c16a6
priya2002hello <nikampriyanka2002@gmail.com>
2022-04-28 20:51:26 +0530

    Comments added to code

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -14,21 +14,22 @@ class MainActivity : AppCompatActivity() {
         val rollbutton: Button = findViewById(R.id.button3)
         rollbutton.setOnClickListener {
             val toast = Toast.makeText(this, "Dice rolled! hurray", Toast.LENGTH_SHORT).show()
-            /*Toast shows a small pop up for few seconds and then vanishes.
-             val result_text:TextView=findViewById(R.id.textView)//creating reference of textview
-            var num=(1..6).random()
-            result_text.text=num.toString()//setting text in Textview*/
+            /*Toast shows a small pop up for few seconds and then vanishes*/
             class dice(val sides: Int) {
                 fun roll(): Int {
                     return (1..sides).random()
                 }
             }
-            // val dice2=dice(6)
+
+            //create objects of class dice.
             val dice2=dice(6)
             val dice1 = dice(6)
             val dice_image:ImageView=findViewById(R.id.diceimg)
             val dice_img2:ImageView=findViewById(R.id.diceimg2)
+
+            //random number between 1-6 gets stored in num.
             val num = dice1.roll()
+            //set image of dice1 according to number appeared on it's top.
             when(num){
                 1->dice_image.setImageResource(R.drawable.dice_1)
                 2->dice_image.setImageResource(R.drawable.dice_2)
@@ -37,7 +38,10 @@ class MainActivity : AppCompatActivity() {
                 5->dice_image.setImageResource(R.drawable.dice_5)
                 6->dice_image.setImageResource(R.drawable.dice_6)
             }
+
+            //random number between 1-6 gets stored in num1
             val num1=dice2.roll()
+            //set image of dice2 according to number appeared on it's top.
             when(num1){
                 1->dice_img2.setImageResource(R.drawable.dice_1)
                 2->dice_img2.setImageResource(R.drawable.dice_2)
@@ -46,30 +50,7 @@ class MainActivity : AppCompatActivity() {
                 5->dice_img2.setImageResource(R.drawable.dice_5)
                 6->dice_img2.setImageResource(R.drawable.dice_6)
             }
-           /* val result_text: TextView = findViewById(R.id.textView) //creating reference of textview
-            val result_text1:TextView=findViewById(R.id.textView2)
-            result_text1.text=num1.toString()
-            result_text.text = num.toString()//setting text in Textview*/
-            /*val lucky:TextView=findViewById(R.id.textView3)
-            val Lucky_num=5
-            val equal:TextView=findViewById(R.id.ifelse1)
-            when statement is used to execute code based on matching input and output.If input provided matches
-            * with ouput then code executes and we come out of when.it is like if else statements
-            when(num){
-                Lucky_num -> lucky.text="You won"
-                1-> lucky.text="You loose"
-                2-> lucky.text="You loose"
-                3->lucky.text="You loose"
-                4->lucky.text="You loose"
-                6-> lucky.text="You loose"
-            }
-            if (num==num1){
-                equal.text="you have same output"
 
-            }
-            else{
-                equal.text=" "
-            }*/
         }
     }
 }
